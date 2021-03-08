3556ff330a566f0f815b25fd1f2ea4146e923269
visaviweb <>
2021-03-08 15:17:23 +0100

    design single illustration

--- single.php
+++ single.php
@@ -12,9 +12,15 @@
 $context         = Timber::context();
 $timber_post     = Timber::get_post();
 $context['post'] = $timber_post;
+$templates = array( 'single-' . $timber_post->ID . '.twig', 'single-' . $timber_post->post_type . '.twig', 'single-' . $timber_post->slug . '.twig', 'single.twig');
+
+
+if ($timber_post->post_type == 'illustrazione') {
+	array_unshift( $templates, 'single-illustrazione.twig' );
+} 
 
 if ( post_password_required( $timber_post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
 } else {
-	Timber::render( array( 'single-' . $timber_post->ID . '.twig', 'single-' . $timber_post->post_type . '.twig', 'single-' . $timber_post->slug . '.twig', 'single.twig' ), $context );
+	Timber::render( $templates, $context );
 }
